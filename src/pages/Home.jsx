dbd1db8c3bcb882dd84713ab4532722fbbc40a14
alexquar <alexanderquarrie@gmail.com>
2024-03-27 20:33:22 -0400

    params

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,22 @@
+import { useFetch } from "../hooks/useFetch"
+import { Link } from "react-router-dom"
+import './Home.css'
 export default function Home() { 
+  const url = 'http://localhost:3000/articles'
+const {data:articles,isPending,error} = useFetch(url)
+
      return (
-    <div>
-      <h2>Home</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime delectus vero nobis nostrum sunt, eos modi sapiente quod alias nulla dolorem deleniti amet minus officiis totam explicabo iusto a sint.</p>
+    <div className="home">
+      <h2>Articles</h2>
+      {isPending && (<div> Loading... </div>)}
+      {error  && (<div> {error}</div>)}
+      {articles && articles.map((article) =>(
+        <div key={article.id} className="card">
+          <h3>{article.title}</h3>
+          <p>{article.author}</p>
+          <Link to={`/articles/${article.id}`}> Read More...</Link>
+           </div>
+      ))}
     </div>
   )
 }
