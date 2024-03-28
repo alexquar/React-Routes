f8058d0343ff046ae6e2d1e56e023d7cd9fb2be3
alexquar <alexanderquarrie@gmail.com>
2024-03-27 20:41:14 -0400

    article page

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,8 +1,20 @@
-
+import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
 export default function Article() {
+    const {id} = useParams()
+const url = `http://localhost:3000/articles/${id}`
+const {data:article,isPending,error} = useFetch(url)
   return (
     <div>
-      Article
-    </div>
+    {isPending && <p>Loading...</p>}
+    {error && <p>{error}</p>}
+    {article && (
+      <div key={article.id}>
+        <h2>{article.title}</h2>
+        <p>By {article.author}</p>
+        <p>{article.body}</p>
+      </div>
+    )}
+  </div>
   )
 }
