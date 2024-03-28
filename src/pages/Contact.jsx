63e9353ff76e5e634a65d552b3b92c6772457494
alexquar <alexanderquarrie@gmail.com>
2024-03-27 21:14:30 -0400

    query

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,16 @@
+import { useLocation } from "react-router-dom"
 
 export default function Contact() {
+  const queryString = useLocation().search
+  // console.log(queryString)
+  
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get("name")
+  
   return (
     <div>
-      <h2>Contact</h2>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime delectus vero nobis nostrum sunt, eos modi sapiente quod alias nulla dolorem deleniti amet minus officiis totam explicabo iusto a sint.</p>
+      <h2>Hey {name}, Contact Us</h2>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam explicabo laudantium nemo voluptas cum omnis error voluptate. Nihil numquam ipsum necessitatibus hic odit neque consequuntur dolor. Magni quos ratione iste.</p>
     </div>
   )
 }
